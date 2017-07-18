d7e07b582f806ec8c8a8ea6d26e89533ca0de184
lytles@takashi <srl.source@nqzero.com>
2017-07-18 12:18:10 -0400

    matter.rest - add TeamNameExists manually

---
+++ src/main/java/mm/rest/TeamsNameExistsReps.java
@@ -0,0 +1,13 @@
+
+package mm.rest;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class TeamsNameExistsReps {
+
+    @SerializedName("exists")
+    @Expose
+    public boolean exists;
+
+}
