579d17bbfd3a2247441c9ce5ccd91a51a1054cd9
lytles@takashi <srl.source@nqzero.com>
2017-09-20 10:55:23 -0400

    matter.jetty - strip down jetty looper to just proxy to matterkilim and doc jetty proxy shortcomings

--- src/main/java/foobar/JettyLooper.java
+++ src/main/java/foobar/JettyLooper.java
@@ -1,77 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package foobar;
 
-import static foobar.MatterServlet.add;
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.eclipse.jetty.client.api.Response;
+import org.eclipse.jetty.proxy.ProxyServlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.Callback;
-import org.srlutils.Simple;
 
-// has not been updated for the kilim server move to MatterFull
-// ie, the kilim portion probably doesn't work
+// proxies to a single server (kilim)
+// which isn't especially useful
+// but demonstrates that the process works
+// and in theory could probably proxy some api calls to a different server
+// unfortunately jetty doesn't support proxying ws
+// which mattermost needs, so this is effectively DOA
 
-public class JettyLooper extends HttpServlet {
-    static String proxyPrefix = "/proxy";
+public class JettyLooper {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(9090);
+        MatterFull.main(args);
+        Server server = new Server(9092);
         ServletContextHandler context = new ServletContextHandler();
         context.setContextPath("/");
 
-        String base = "/home/lytles/working/fun/chernika/mattermost/webapp/dist/";
-
-        add("/static/*",base,context);
-        context.setWelcomeFiles(new String[] {"root.html"});
-//        add("/*",base,context);
-
-        MatterControl mm = new MatterControl();
-        ServletHolder wsHolder = new ServletHolder("echo",mm.ws);
-        context.addServlet(wsHolder,"/api/*");
-
-        KilimProxy kproxy = new KilimProxy();
-        JettyLooper looper = new JettyLooper();
-        ServletHolder mh = new ServletHolder(looper);
-        ServletHolder pk = new ServletHolder(kproxy);
+        ServletHolder pk = new ServletHolder(new ProxyServlet.Transparent());
         pk.setInitParameter("proxyTo","http://localhost:9091");
-        mh.setInitParameter("resourceBase",base);
-        mh.setInitParameter("redirectWelcome","false");
-//        context.addServlet(mh,"/api/*");
-        context.addServlet(pk,proxyPrefix+"/*");
-        add("/*",base,context);
+        context.addServlet(pk,"/*");
 
         server.setHandler(context);
         server.start();
-        
-        
     }
-    protected void service(HttpServletRequest req,HttpServletResponse resp) throws ServletException,IOException {
-        String url = req.getRequestURI();
-        System.out.println("matter: " + url);
-        getServletContext().getRequestDispatcher(proxyPrefix+url).forward(req,resp);
-    }
-    public static class KilimProxy extends org.eclipse.jetty.proxy.ProxyServlet.Transparent {
-        protected void onResponseContent(
-                HttpServletRequest req,HttpServletResponse resp,
-                Response proxy, byte[] buffer,int offset,int length,Callback callback) {
-            System.out.println("proxy: "+req.getRequestURI());
-            System.out.println(new String(buffer));
-            System.out.format("----------------------------------------------------------------------------\n\n");
-            super.onResponseContent(req,resp,proxy,buffer,offset,length,callback);
-        }
-        protected String rewriteTarget(HttpServletRequest request) {
-            String url = super.rewriteTarget(request);
-            return url.replaceFirst(proxyPrefix,"");
-        }
-    }
-    
 }
